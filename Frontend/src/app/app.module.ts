921d149a39d1954a7c75a8c4dd46dde7d32ef3a2
Juliana Ribeiro <julianabr2607@gmail.com>
2018-11-23 17:21:48 -0200

    Update app.module.ts

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,16 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule }    from '@angular/common/http';
 import { UsuarioComponent } from './usuario/usuario.component';
-<<<<<<< HEAD
 import { UsuarioService } from './Services/usuario.service';
-=======
-import { UsuarioService } from './usuario.service';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RedefineSenhaComponent } from './redefine-senha/redefine-senha.component';
->>>>>>> 82e461f9cfe312410264c07b6c6db3054e8fc0e2
 
 @NgModule({
   declarations: [
